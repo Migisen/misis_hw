7e08dd6ad44216b97a9e2d97c978c4acbbd75766
Migisen <emelds1@yandex.ru>
2022-12-27 00:34:26 +0300

    hw5 complete

--- hw5/task2.py
+++ hw5/task2.py
@@ -1,16 +1,24 @@
-def move(n: int, x: int, y: int) -> None:
+def move(n: int, x: str, y: str, z: str) -> None:
     """Перекладывание дисков пирамиды со стержня под номером x
     на стержень под номером y
 
     Args:
         n (int): высота пирамиды
-        x (int): номер изначального стержня
-        y (int): номер стержня на который перекладывают диск
+        x (str): изначальный стержень
+        y (str): стержень на который перекладывают диск
+        z (str): "дополнительный стержень"
     """
-    ...
+    rod_mapping = {'A': 1,
+                   'B': 2,
+                   'C': 3}
+    if n == 1:
+        print(f'1 {rod_mapping[x]} {rod_mapping[y]}')
+        return
+    move(n-1, x, z, y)
+    print(f'{n} {rod_mapping[x]} {rod_mapping[y]}')
+    move(n-1, z, y, x)
 
 
 if __name__ == "__main__":
     N = 3
-    A = list(range(N, 0, -1))
-    B, C = [], []
+    move(N, 'A', 'C', 'B')
